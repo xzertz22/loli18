dbb23621b9717fbf2a6b07c818e67fcce0125780
xzertz22 <67366974+xzertz22@users.noreply.github.com>
2020-06-25 19:31:18 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,68 @@
+'''
+Layscape Kumaru Script
+My Principal Anyone Can Enjoy Premium Feature Without Buy It ^.^
+
+Any Suggest PM Me On Whatsapp or Telegram
+'''
+
+
+'''
+Fill Your 999Doge Account
+'''
+account = {
+	"Username":"xloli18",
+	"Password":"asdasd"
+}
+'''
+Change Your Bet Strategy/Bet Setting
+'''
+betset = {
+	"Basebet":"0.001",
+	"Chance1":"81",
+	"Chance2":"90"
+}
+
+'''
+Select Bet Setting Tools
+Note :
+IFLose = Increase Bet After Lose
+IFWin = Increase Bet After Win
+'''
+tools = {
+	"IFLose":"3.5",
+	"IFWin":"2.8",
+	"TargetProfit":"1000",
+	"TargetLose":"99999999999",
+	"ResetBetIfWin":"2",
+	"ResetBetIfLose":"0",
+	"ResetSeedEachBet":"ON"
+}
+
+'''
+Select Premium Feature ^.^
+Note :
+Interval In Second, Comma Allowed
+RefreshIfWin and RefreshIfLose Use Number to Use
+Example 
+RefreshIfWin : 3. Automaticaly Refresh After 3 Win Streak OFF to OFF
+RefreshIfLose : 3. Automaticaly Refresh After 3 Lose Streak OFF to OFF
+'''
+feature = {
+	"BetInterval":"0",
+	"BetIntervalLose":"0",
+	"BetInvervalWin":"0",
+	"IntervalRefresh":"0",
+	"RefreshIfWin":"OFF",
+	"RefreshIfLose":"OFF",
+	"AutoRefresh":"ON"
+}
+
+'''
+Dont Forget To Change Your Wallet Here
+'''
+withdraw = {
+	"AutoWithdraw":"OFF",
+	"AmountWithdraw":"5",
+	"WithdrawIfBalance":"5",
+	"Address":"D916W29eCE6zvryu8wGUs7XYHiqhjmrEyd"
+}
